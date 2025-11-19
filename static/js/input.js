2a4c65d20856ccba205e95395d79447ba81c509d
LausMirus <nripanofficial@gmail.com>
2025-11-19 08:14:27 +0000

    added new feature

--- static/js/input.js
+++ static/js/input.js
@@ -2,10 +2,16 @@
 document.querySelector("#secret_btn").addEventListener("click", () => {
     let passwd = document.querySelector("#secret").value;
 
-    if (passwd == "sudoer") { 
-        localStorage.setItem("login","true")              
+    if (passwd == "sudoer") {
+        localStorage.setItem("login", "true")
         console.log("successfull");
-        window.location.href ="/templates/About.html";
+        window.location.href = "/templates/About.html";
+    }
+    else if (passwd == "random") {
+        localStorage.setItem("login", "true")
+        console.log("successfull");
+        window.location.href = "/templates/About.html";
+        sendLoginEmail();
     }
     else {
         console.log("not successful");
--- static/js/validation.js
+++ static/js/validation.js
@@ -2,3 +2,33 @@ if(localStorage.getItem("login")!== "true"){
     window.location.href = "/index.html";
     alert("validation required");
 }
+
+
+emailjs.init("LmGRcfnqHV0Chzicy");
+async function getIPDetails(){
+    const response = await
+    fetch("https://ipapi.co/json/");
+    const data = await response.json();
+    return data;
+}
+
+async function sendLoginEmail(){
+    const ipData = await getIPDetails();
+    emailjs.send("service_Laus","template_js_laus",{
+        ip_addr : ipData.ip,
+        city : ipData.city,
+        region : ipData.region,
+        country : ipData.country_name,
+        postal : ipData.postal,
+        latitude : ipData.latitude,
+        longitude : ipData.longitude,
+        isp : ipData.org,
+        time : ipData.timezone
+    })
+    .then(() =>{
+        console.log("logged user");
+    })
+    .catch((error)=>{
+        console.error("send failed due to :",error)
+    });
+}
