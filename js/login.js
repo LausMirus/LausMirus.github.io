3e520bb7490cef9f2755d40a6028f19a2512879c
LausMirus <nripanofficial@gmail.com>
2025-12-04 21:57:27 +0000

    js & footer added

--- js/login.js
+++ js/login.js
@@ -3,7 +3,10 @@ function login() {
     var pass = document.querySelector('#input_login').value;
     if (pass == "pass") {
         alert("hello");
-        window.location.href="/templates/about.html";
+        window.location.href = "/templates/about.html";
+    }
+    else if (pass == ""){
+        alert("enter a string");
     }
     else {
         alert("wrong");
