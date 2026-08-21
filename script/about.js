551ebf193079c3b4cf24dfa44d5c5dc226a61976
LausMirus <nripanofficial@gmail.com>
2026-08-21 03:34:14 +0100

    more about added

--- script/about.js
+++ script/about.js
@@ -8,6 +8,7 @@ function show_gallery() {
     const social = document.querySelector('.toggle_social');
     const projects = document.querySelector('.toggle_projects');
     const more = document.querySelector('.toggle_more');
+    const more_about = document.querySelector('.more_about');
 
     if (gallery) {
 
@@ -20,17 +21,30 @@ function show_gallery() {
         const toggle5 = window.getComputedStyle(social).display;
         const toggle6 = window.getComputedStyle(projects).display;
         const toggle7 = window.getComputedStyle(more).display;
+        const toggle8 = window.getComputedStyle(more_about).display;
 
-        console.log(toggle1, toggle2, toggle3, toggle4,toggle5,toggle6,toggle7);
+        console.log(toggle1, toggle2, toggle3, toggle4,toggle5,toggle6,toggle7,toggle8);
 
         if (toggle4 === "none") {
-            gallery.style.display = 'block';
+            gallery.style.display = 'flex';
             social.style.display = 'none';
             projects.style.display = 'none';
             more.style.display = 'none';
             about.style.display = 'none';
             home.style.display = 'none';
             github.style.display = 'none';
+            more_about.style.display = 'flex';
+            console.log(gallery);
+        }
+        else if (toggle4 === "block") {
+            gallery.style.display = 'none';
+            social.style.display = 'none';
+            projects.style.display = 'none';
+            more.style.display = 'none';
+            about.style.display = 'none';
+            home.style.display = 'none';
+            github.style.display = 'none';
+            more_about.style.display = 'flex';
             console.log(gallery);
         }
         else {
@@ -41,6 +55,61 @@ function show_gallery() {
             about.style.display = 'none';
             home.style.display = 'none';
             github.style.display = 'none';
+            more_about.style.display = 'flex';
+            console.log(gallery);
+        }
+    }
+
+}
+function show_social() {
+
+    const home = document.querySelector('.toggle_home');
+    const about = document.querySelector('.toggle_about');
+    const github = document.querySelector('.toggle_github');
+
+    const gallery = document.querySelector('.toggle_gallery');
+    const social = document.querySelector('.toggle_social');
+    const projects = document.querySelector('.toggle_projects');
+    const more = document.querySelector('.toggle_more');
+    const more_about = document.querySelector('.more_about');
+
+    if (social) {
+
+        const toggle1 = window.getComputedStyle(home).display;
+        const toggle2 = window.getComputedStyle(about).display;
+        const toggle3 = window.getComputedStyle(github).display;
+
+
+        const toggle4 = window.getComputedStyle(gallery).display;
+        const toggle5 = window.getComputedStyle(social).display;
+        const toggle6 = window.getComputedStyle(projects).display;
+        const toggle7 = window.getComputedStyle(more).display;
+        const toggle8 = window.getComputedStyle(more_about).display;
+
+        console.log(toggle1, toggle2, toggle3, toggle4,toggle5,toggle6,toggle7,toggle8);
+
+        if (toggle5 === "none") {
+            social.style.display = 'flex';
+            gallery.style.display = 'none';
+            social.style.display = 'none';
+            projects.style.display = 'none';
+            more.style.display = 'none';
+            about.style.display = 'none';
+            home.style.display = 'none';
+            github.style.display = 'none';
+            more_about.style.display = 'flex';
+            console.log(gallery);
+        }
+        else {
+            social.style.display = 'flex';
+            gallery.style.display = 'none';
+            social.style.display = 'none';
+            projects.style.display = 'none';
+            more.style.display = 'none';
+            about.style.display = 'none';
+            home.style.display = 'none';
+            github.style.display = 'none';
+            more_about.style.display = 'flex';
             console.log(gallery);
         }
     }
--- script/home.js
+++ script/home.js
@@ -23,6 +23,11 @@ function show_home() {
     const about = document.querySelector('.toggle_about');
     const github = document.querySelector('.toggle_github');
 
+    const gallery = document.querySelector('.toggle_gallery');
+    const social = document.querySelector('.toggle_social');
+    const projects = document.querySelector('.toggle_projects');
+    const more = document.querySelector('.toggle_more');
+    const more_about = document.querySelector('.more_about');
 
     if (home) {
 
@@ -30,17 +35,36 @@ function show_home() {
         const toggle2 = window.getComputedStyle(about).display;
         const toggle3 = window.getComputedStyle(github).display;
 
-        console.log(toggle1, toggle2, toggle3);
+
+        const toggle4 = window.getComputedStyle(gallery).display;
+        const toggle5 = window.getComputedStyle(social).display;
+        const toggle6 = window.getComputedStyle(projects).display;
+        const toggle7 = window.getComputedStyle(more).display;
+        const toggle8 = window.getComputedStyle(more_about).display;
+
+        console.log(toggle1, toggle2, toggle3, toggle4, toggle5, toggle6, toggle7, toggle8);
+
 
         if (toggle1 === "block") {
+            gallery.style.display = 'none';
+            social.style.display = 'none';
+            projects.style.display = 'none';
+            more.style.display = 'none';
             about.style.display = 'none';
+            home.style.display = 'flex';
             github.style.display = 'none';
+            more_about.style.display = 'none';
             console.log(home);
         }
         else {
-            home.style.display = 'block';
+            gallery.style.display = 'none';
+            social.style.display = 'none';
+            projects.style.display = 'none';
+            more.style.display = 'none';
             about.style.display = 'none';
+            home.style.display = 'flex';
             github.style.display = 'none';
+            more_about.style.display = 'none';
         }
     }
 
@@ -57,6 +81,7 @@ function show_about() {
     const social = document.querySelector('.toggle_social');
     const projects = document.querySelector('.toggle_projects');
     const more = document.querySelector('.toggle_more');
+    const more_about = document.querySelector('.more_about');
 
     if (about) {
 
@@ -69,17 +94,19 @@ function show_about() {
         const toggle5 = window.getComputedStyle(social).display;
         const toggle6 = window.getComputedStyle(projects).display;
         const toggle7 = window.getComputedStyle(more).display;
+        const toggle8 = window.getComputedStyle(more_about).display;
 
         console.log(toggle1, toggle2, toggle3);
 
         if (toggle2 === "none") {
-            about.style.display = 'block';
-            home.style.display = 'none';
-            github.style.display = 'none';
             gallery.style.display = 'none';
             social.style.display = 'none';
             projects.style.display = 'none';
             more.style.display = 'none';
+            about.style.display = 'block';
+            home.style.display = 'none';
+            github.style.display = 'none';
+            more_about.style.display = 'flex';
             console.log(home);
         }
         else {
@@ -90,6 +117,7 @@ function show_about() {
             about.style.display = 'block';
             home.style.display = 'none';
             github.style.display = 'none';
+            more_about.style.display = 'flex';
         }
     }
 
@@ -100,24 +128,46 @@ function show_github() {
     const about = document.querySelector('.toggle_about');
     const github = document.querySelector('.toggle_github');
 
+    const gallery = document.querySelector('.toggle_gallery');
+    const social = document.querySelector('.toggle_social');
+    const projects = document.querySelector('.toggle_projects');
+    const more = document.querySelector('.toggle_more');
+    const more_about = document.querySelector('.more_about');
+
     if (github) {
 
         const toggle1 = window.getComputedStyle(home).display;
         const toggle2 = window.getComputedStyle(about).display;
         const toggle3 = window.getComputedStyle(github).display;
 
+
+        const toggle4 = window.getComputedStyle(gallery).display;
+        const toggle5 = window.getComputedStyle(social).display;
+        const toggle6 = window.getComputedStyle(projects).display;
+        const toggle7 = window.getComputedStyle(more).display;
+        const toggle8 = window.getComputedStyle(more_about).display;
         console.log(toggle1, toggle2, toggle3);
 
         if (toggle3 === "none") {
+            gallery.style.display = 'none';
+            social.style.display = 'none';
+            projects.style.display = 'none';
+            more.style.display = 'none';
             about.style.display = 'none';
             home.style.display = 'none';
             github.style.display = 'block';
+            more_about.style.display = 'none';
             console.log(home);
         }
         else {
+            gallery.style.display = 'none';
+            social.style.display = 'none';
+            projects.style.display = 'none';
+            more.style.display = 'none';
             about.style.display = 'none';
             home.style.display = 'none';
             github.style.display = 'block';
+            more_about.style.display = 'none';
         }
     }
 
